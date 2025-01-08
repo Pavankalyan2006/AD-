17e3899f1cae870acca0f2d78d92a46c52266399
Vagicherla Pavan kalyan <153082616+Pavankalyan2006@users.noreply.github.com>
2025-01-08 23:19:54 +0530

    Add files via upload

---
+++ Day-5-1.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1, n + 1):
+    print(i)
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print( sum)
---
+++ Day-5-2.py
@@ -0,0 +1,5 @@
+def calculate_square(n):
+    return n ** 2
+num = int(input("Enter"))
+result = calculate_square(num)
+print("The square of", num, "is:", result)
