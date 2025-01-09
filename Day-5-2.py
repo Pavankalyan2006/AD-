5b9f355b386e2d6879d7d33b216d835f2f568bb5
Pavankalyan2006 <pavankalyanvagicharla@gmail.com>
2025-01-09 19:23:00 +0530

    Initial commit with added

--- Day-5-2.py
+++ Day-5-2.py
@@ -3,3 +3,9 @@ def calculate_square(n):
 num = int(input("Enter"))
 result = calculate_square(num)
 print("The square of", num, "is:", result)
+def calculate_square(n):
+    return n ** 2
+
+num = int(input("Enter a number: "))
+result = calculate_square(num)
+print(f"The square of {num} is: {result}")
